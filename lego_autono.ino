35456aa4cc8c64c5a50dd6e731ec949b39383e3e
Blake McIvor <bmcivor56@gmail.com>
2016-03-19 01:38:13 +1000

    project init

---
+++ lego_autono.ino
@@ -0,0 +1,21 @@
+/************************************************************************/
+/*                                                                 
+  This application has been authored with the purpose of interfacing 
+  microcontrollers such as an Arduino and Raspberry Pi 2 Model B to 
+  control the outputs to the motors of a car - 
+  -> Left Rear 9V DC Motor (Drive)
+  -> Right Rear 9V DC Motor (Drive)
+  -> Front Servo Motor (Steering)
+  
+  based on the inputs of the sensors - 
+  
+  ->
+  ->
+  ->
+  
+************************************************************************/
+/*
+
+
+
+
