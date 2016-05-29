339e4bee061af4277b158e5347b8c51d27c373bd
Blake McIvor <bmcivor56@gmail.com>
2016-05-29 14:41:48 +1000

    setting training harness to gather data

---
+++ controller/collect_data.py
@@ -0,0 +1,16 @@
+
+"""
+Collects data from training exercises and calibration to perform autonomous
+driving of Lego Autono
+"""
+
+import cv2
+import numpy as np
+import serial
+import pygame
+import socket
+
+
+
+if __name__ == '__main__':
+    CollectData()
